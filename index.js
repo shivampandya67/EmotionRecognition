2b64eb8d590f65d18c48a337a2d5271406ac47c0
shivampandya67 <39360502+shivampandya67@users.noreply.github.com>
2023-11-27 12:29:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,54 +12,20 @@ function convertImageToTensor(image) {
             imgElement.src = e.target.result;
 
             imgElement.onload = () => {
-                // Create a canvas with a fixed size (e.g., 48x48)
                 const canvas = document.createElement('canvas');
-                const targetSize = 48;
-
-                // Resize the image while maintaining aspect ratio
-                const scaleFactor = Math.min(targetSize / imgElement.width, targetSize / imgElement.height);
-                const newWidth = imgElement.width * scaleFactor;
-                const newHeight = imgElement.height * scaleFactor;
-                canvas.width = targetSize;
-                canvas.height = targetSize;
-
-                // Center the image on the canvas
-                const xOffset = (targetSize - newWidth) / 2;
-                const yOffset = (targetSize - newHeight) / 2;
-
-                // Draw the image on the canvas
+                canvas.width = 48;
+                canvas.height = 48;
                 const ctx = canvas.getContext('2d');
-                ctx.fillStyle = 'white';
-                ctx.fillRect(0, 0, canvas.width, canvas.height);
-                ctx.drawImage(imgElement, xOffset, yOffset, newWidth, newHeight);
+
+                ctx.drawImage(imgElement, 0, 0, canvas.width, canvas.height);
 
                 const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-                const normalizedData = imageData.map(value => value / 255.0);
+                const tensorData = new Float32Array(imageData);
 
                 // Assuming your model expects the input size [1, 3, 48, 48]
                 const expectedDims = [1, 3, 48, 48];
-                
 
-                // Debugging output
-                console.log('Normalized:', normalizedData.length);
-                console.log('Expected:', expectedDims.reduce((a, b) => a * b, 1));
-
-                // Ensure that the normalized data length matches the expected size
-                if (normalizedData.length !== expectedDims.reduce((a, b) => a * b, 1)) {
-                    reject(new Error('Input dims do not match data length.'));
-                    return;
-                }
-
-                // Create a Float32Array with the correct size
-                const tensorData = new Float32Array(expectedDims.reduce((a, b) => a * b, 1));
-
-                // Copy the normalized data to the tensor data, skipping the alpha channel
-                for (let i = 0; i < normalizedData.length / 4; i++) {
-                    for (let j = 0; j < 3; j++) {
-                        tensorData[i * 3 + j] = normalizedData[i * 4 + j];
-                    }
-                }
-
+                // Reshape the tensor data to match the expected input size
                 const tensor = new onnx.Tensor(tensorData, 'float32', expectedDims);
 
                 resolve(tensor);
