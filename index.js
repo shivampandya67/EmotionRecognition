c19502382d66e29b3b2849a2b3fa58788634dafe
shivampandya67 <39360502+shivampandya67@users.noreply.github.com>
2023-11-28 06:11:50 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,6 @@
-function convertImageToTensor(image) {
-    return new Promise((resolve, reject) => {
-        if (!(image instanceof Blob)) {
-            reject(new Error('Invalid image parameter'));
-            return;
-        }
-
-        const reader = new FileReader();
-
-        reader.onload = function (e) {
-            const imgElement = new Image();
-            imgElement.src = e.target.result;
-
-            imgElement.onload = () => {
-                const canvas = document.createElement('canvas');
-                canvas.width = 48;
-                canvas.height = 48;
-                const ctx = canvas.getContext('2d');
-
-                ctx.drawImage(imgElement, 0, 0, canvas.width, canvas.height);
-
-                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-
-                // Assuming your model expects the input size [1, 3, 48, 48]
-                const expectedDims = [1, 3, 48, 48];
-
-                // Create the tensor directly from Float32Array
-                const tensorData = new Float32Array(imageData);
-                const tensor = new onnx.Tensor(tensorData, 'float32', expectedDims);
-
-                resolve(tensor);
-            };
-        };
-
-        reader.onerror = function (error) {
-            reject(error);
-        };
-
-        reader.readAsDataURL(image);
-    });
-}
-
-// Create an ONNX Runtime session
-const session = new onnx.InferenceSession();
-
 // Load the ONNX model
-session.loadModel("emotion_recognition_model.onnx")
-    .then(() => {
-        console.log("Model loaded successfully.");
-    })
-    .catch((error) => {
-        console.error("Error loading the model:", error);
-    });
+const session = new onnx.InferenceSession();
+session.loadModel("emotion_recognition_model.onnx");
 
 // Function to run inference on the uploaded image
 async function runInference() {
@@ -77,14 +27,44 @@ async function runInference() {
 }
 
 // Function to preprocess the image
-async function preprocessImage(image) {
-    // Convert the image to a tensor (implement your logic here)
-    // Example: Assuming you have a function to convert image to tensor
-    const tensor = await convertImageToTensor(image);
+async function convertImageToTensor(image) {
+    return new Promise((resolve, reject) => {
+        if (!(image instanceof Blob)) {
+            reject(new Error('Invalid image parameter'));
+            return;
+        }
 
-    // Implement any required preprocessing steps
+        const reader = new FileReader();
 
-    return tensor;
+        reader.onload = function (e) {
+            const imgElement = new Image();
+            imgElement.src = e.target.result;
+
+            imgElement.onload = () => {
+                const canvas = document.createElement('canvas');
+                canvas.width = 48;
+                canvas.height = 48;
+                const ctx = canvas.getContext('2d');
+
+                ctx.drawImage(imgElement, 0, 0, canvas.width, canvas.height);
+
+                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+                const tensorData = new Float32Array(imageData);
+
+                // Assuming your model expects the input size [1, 3, 48, 48]
+                const expectedDims = [1, 3, 48, 48];
+                const tensor = new onnx.Tensor(tensorData, 'float32', expectedDims);
+
+                resolve(tensor);
+            };
+        };
+
+        reader.onerror = function (error) {
+            reject(error);
+        };
+
+        reader.readAsDataURL(image);
+    });
 }
 
 // Function to process the output tensor
