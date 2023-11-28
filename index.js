9ebfa9da483cdcec7966cf92d8d5dc37ad223348
shivampandya67 <39360502+shivampandya67@users.noreply.github.com>
2023-11-28 06:28:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,98 +1,80 @@
-// Load the ONNX model
-const session = new onnx.InferenceSession();
-session.loadModel("emotion_recognition_model.onnx");
+// Function to preview the uploaded image
+function previewImage(event) {
+  const preview = document.getElementById('preview');
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onloadend = function() {
+    preview.src = reader.result;
+    preview.style.display = 'block';
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = '';
+    preview.style.display = 'none';
+  }
+}
 
 // Function to run inference on the uploaded image
 async function runInference() {
-    // Get the input image from the file input
-    const fileInput = document.getElementById("imageInput");
-    const image = fileInput.files[0];
+  const preview = document.getElementById('preview');
+  if (!preview.src || preview.src === '#') {
+    alert('Please upload an image first.');
+    return;
+  }
 
-    try {
-        // Preprocess the image (convert to tensor, resize, normalize, etc.)
-        const tensor = await convertImageToTensor(image);
+  const tensorX = await preprocessImage(preview); // Preprocess the image
 
-        // Run inference
-        const outputTensor = await session.run([tensor]);
+  try {
+    const session = await ort.InferenceSession.create('EmotionCnnOnnx.onnx');
+    const result = await session.run({ input: tensorX });
+    const outputData = result.output.data;
 
-        // Process the output (interpret the result)
-        const predictedEmotion = processOutput(outputTensor);
-
-        // Display the result on the webpage
-        const outputDiv = document.getElementById("output");
-        outputDiv.innerHTML = `Predicted Emotion: ${predictedEmotion}`;
-    } catch (error) {
-        console.error("Error during inference:", error);
-    }
+    // Process outputData to get predicted class, then display the results
+    displayResults(outputData); // Function to display results (defined below)
+  } catch (error) {
+    console.error('Error running inference:', error);
+    alert('Failed to run inference. Please try again.');
+  }
 }
 
-// Function to preprocess the image
-async function convertImageToTensor(image) {
-    return new Promise((resolve, reject) => {
-        if (!(image instanceof Blob)) {
-            reject(new Error('Invalid image parameter'));
-            return;
-        }
+// Function to preprocess the uploaded image
+async function preprocessImage(imageElement) {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
 
-        const reader = new FileReader();
+  canvas.width = 48;
+  canvas.height = 48;
 
-        reader.onload = function (e) {
-            const imgElement = new Image();
-            imgElement.src = e.target.result;
+  ctx.drawImage(imageElement, 0, 0, canvas.width, canvas.height);
 
-            imgElement.onload = () => {
-                console.log('Image loaded successfully.');
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  const tensorData = new Float32Array(3 * 48 * 48);
 
-                const canvas = document.createElement('canvas');
-                canvas.width = 48;
-                canvas.height = 48;
-                const ctx = canvas.getContext('2d');
+  for (let i = 0; i < imageData.length; i += 4) {
+    const r = imageData[i] / 255;
+    const g = imageData[i + 1] / 255;
+    const b = imageData[i + 2] / 255;
 
-                ctx.drawImage(imgElement, 0, 0, canvas.width, canvas.height);
+    tensorData[i / 4] = r;
+    tensorData[(i / 4) + (48 * 48)] = g;
+    tensorData[(i / 4) + (2 * 48 * 48)] = b;
+  }
 
-                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  const tensorX = new ort.Tensor('float32', tensorData, [1, 3, 48, 48]);
 
-                // Assuming your model expects the input size [1, 3, 48, 48]
-                const expectedDims = [1, 3, 48, 48];
-
-                console.log('Image Data Length:', imageData.length);
-                console.log('Expected Dims:', expectedDims);
-
-                // Ensure that the tensor data length matches the expected input size
-                if (imageData.length !== expectedDims.reduce((a, b) => a * b, 1)) {
-                    console.error('Error: Input dims do not match data length');
-                    reject(new Error('Input dims do not match data length'));
-                    return;
-                }
-
-                // Convert the data to a Float32Array
-                const tensorData = new Float32Array(imageData);
-
-                console.log('Reshaped Tensor Data Length:', tensorData.length);
-
-                const tensor = new onnx.Tensor(tensorData, 'float32', expectedDims);
-
-                console.log('Tensor created successfully.');
-                resolve(tensor);
-            };
-        };
-
-        reader.onerror = function (error) {
-            console.error('Error reading image:', error);
-            reject(error);
-        };
-
-        reader.readAsDataURL(image);
-    });
+  return tensorX;
 }
 
-// Function to process the output tensor
-function processOutput(outputTensor) {
-    // Implement logic to interpret the output tensor
-    // Example: Assuming outputTensor is a Float32Array with confidence scores
-    const maxIndex = outputTensor.indexOf(Math.max(...outputTensor));
-    const emotionClasses = ["Happy", "Sad"];  // Replace with your actual class names
-    const predictedEmotion = emotionClasses[maxIndex];
+// Function to display the inference results
+function displayResults(outputData) {
+  // Example display of results
+??
+  const classifiedImage = document.getElementById('classifiedImage');
+  const classificationInfo = document.getElementById('classificationInfo');
 
-    return predictedEmotion;
+  classifiedImage.src = document.getElementById('preview').src;
+  classificationInfo.innerText = `Actual Class: ${actualClass}, Predicted Class: ${predictedClass}, Correct: ${isCorrect}`;
 }
