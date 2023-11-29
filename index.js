448aa80061845cae20e1e9c063b774b8cfd20c43
shivampandya67 <39360502+shivampandya67@users.noreply.github.com>
2023-11-29 12:25:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,71 @@
-// Function to preview the uploaded image
-function previewImage(event) {
-  const preview = document.getElementById('preview');
-  const file = event.target.files[0];
-  const reader = new FileReader();
-
-  reader.onloadend = function() {
-    preview.src = reader.result;
-    preview.style.display = 'block';
-  }
-
-  if (file) {
-    reader.readAsDataURL(file);
-  } else {
-    preview.src = '';
-    preview.style.display = 'none';
-  }
-}
+// Load the ONNX model
+const session = new onnx.InferenceSession();
+session.loadModel("emotion_recognition_model.onnx");
 
 // Function to run inference on the uploaded image
 async function runInference() {
-  const preview = document.getElementById('preview');
-  if (!preview.src || preview.src === '#') {
-    alert('Please upload an image first.');
-    return;
-  }
+    // Get the input image from the file input
+    const fileInput = document.getElementById("imageInput");
+    const image = fileInput.files[0];
 
-  const tensorX = await preprocessImage(preview); // Preprocess the image
+    try {
+        // Convert the image to a tensor
+        const tensor = await new Promise((resolve, reject) => {
+            if (!(image instanceof Blob)) {
+                reject(new Error('Invalid image parameter'));
+                return;
+            }
 
-  try {
-    const session = await ort.InferenceSession.create('emotion_recognition_model.onnx');
-    const result = await session.run({ input: tensorX });
-    const outputData = result.output.data;
+            const reader = new FileReader();
 
-    // Process outputData to get predicted class, then display the results
-    displayResults(outputData); // Function to display results (defined below)
-  } catch (error) {
-    console.error('Error running inference:', error);
-    alert('Failed to run inference. Please try again.');
-  }
-}
+            reader.onload = function (e) {
+                const imgElement = new Image();
+                imgElement.src = e.target.result;
 
-// Function to preprocess the uploaded image
-async function preprocessImage(imageElement) {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
+                imgElement.onload = () => {
+                    const canvas = document.createElement('canvas');
+                    canvas.width = 48;
+                    canvas.height = 48;
+                    const ctx = canvas.getContext('2d');
 
-  canvas.width = 48;
-  canvas.height = 48;
+                    ctx.drawImage(imgElement, 0, 0, canvas.width, canvas.height);
 
-  ctx.drawImage(imageElement, 0, 0, canvas.width, canvas.height);
+                    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+                    const normalizedData = imageData.map(value => value / 255.0);
 
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-  const tensorData = new Float32Array(3 * 48 * 48);
+                    // Assuming your model expects the input size [1, 3, 48, 48]
+                    const expectedDims = [1, 3, 48, 48];
 
-  for (let i = 0; i < imageData.length; i += 4) {
-    const r = imageData[i] / 255;
-    const g = imageData[i + 1] / 255;
-    const b = imageData[i + 2] / 255;
+                    // Reshape the normalized data to match the expected input size
+                    const reshapedData = new Float32Array(expectedDims.reduce((a, b) => a * b, 1));
+                    normalizedData.forEach((value, index) => {
+                        reshapedData[index % reshapedData.length] += value;
+                    });
 
-    tensorData[i / 4] = r;
-    tensorData[(i / 4) + (48 * 48)] = g;
-    tensorData[(i / 4) + (2 * 48 * 48)] = b;
-  }
+                    const tensor = new onnx.Tensor(reshapedData, 'float32', expectedDims);
 
-  const tensorX = new ort.Tensor('float32', tensorData, [1, 3, 48, 48]);
+                    resolve(tensor);
+                };
+            };
 
-  return tensorX;
-}
+            reader.onerror = function (error) {
+                reject(error);
+            };
 
-// Function to display the inference results
-function displayResults(outputData) {
-  // Example display of results
-  // ...
-  const classifiedImage = document.getElementById('classifiedImage');
-  const classificationInfo = document.getElementById('classificationInfo');
+            reader.readAsDataURL(image);
+        });
 
-  classifiedImage.src = document.getElementById('preview').src;
-  classificationInfo.innerText = `Actual Class: ${actualClass}, Predicted Class: ${predictedClass}, Correct: ${isCorrect}`;
+        // Run inference
+        const outputTensor = await session.run([tensor]);
+
+        // Process the output tensor and display the result on the webpage
+        const maxIndex = outputTensor[0].data.indexOf(Math.max(...outputTensor[0].data));
+        const emotionClasses = ["Happy", "Sad"];  // Replace with your actual class names
+        const predictedEmotion = emotionClasses[maxIndex];
+
+        const outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = `Predicted Emotion: ${predictedEmotion}`;
+    } catch (error) {
+        console.error("Error during inference:", error);
+    }
 }
